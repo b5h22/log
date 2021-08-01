68751d0058d3c838840e6cc9a6bff3ca4dfa535d
b5h22 <b5h22@Boheeui-MacBookPro.local>
2021-08-01 20:35:31 +0900

    test

---
+++ docs/.vuepress/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="titleContainer">
+        <br>
+      <h1>기록,
+        <br>journey to discover myself
+      </h1>
+
+      <div class="infoContainer">
+        <div class="name">by B5H22</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+@import "https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap";
+.infoContainer {
+  text-align: right;
+}
+h1 {
+  font-family: "Black Han Sans", sans-serif;
+  font-weight: 400;
+  border-bottom: 17px solid rgb(13, 212, 80);
+  word-break: keep-all;
+  padding-bottom: 2rem;
+  font-size: 4rem;
+}
+.name {
+  font-weight: 900;
+  font-size: 2rem;
+}
+ul {
+  list-style: none;
+  line-height: 1.5rem;
+}
+@media (max-width: 550px) {
+  h1 {
+    font-size: 4.4rem;
+  }
+  .name {
+    font-size: 1.7rem;
+  }
+}
+@media (max-width: 430px) {
+  h1 {
+    font-size: 3.4rem;
+  }
+}
+</style>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: "Daily Log",
+  description: "Daily Log - vuepress",
+  themeConfig: {
+    nav: [{ text: "Github", link: "https://github.com/b5h22" }], // github주소
+  },
+  //<username>.github.io 뒤에 주소가 붙으시면 아래와 같이 ..
+  base: "/log/"
+};

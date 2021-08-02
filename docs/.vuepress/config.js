6bef26bd4792ef38e843426004a639cfaab08de7
b5h22 <b5h22@Boheeui-MacBookPro.local>
2021-08-02 14:44:44 +0900

    test01

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,25 @@
 module.exports = {
   title: "Daily Log",
   description: "Daily Log - vuepress",
+  base: "/log/",
   themeConfig: {
     nav: [{ text: "Github", link: "https://github.com/b5h22" }], // github주소
-    sidebar: 'auto', // 사이드바 사용 여부. auto로 하면 content의 내용이 반영된다.
-    smoothScroll: true, // 부드러운 스크롤 사용 여부
+    sidebar: [
+      {
+        type: 'group',
+        title: 'Logbook',
+        collapsable: true,
+        children: [
+          {
+            type: 'group',
+            title: '21-August',
+            collapsable: true,
+            children: [
+              '/logbook/21-August/Aug-08',
+            ]
+          }
+        ],
+      }
+    ]
   },
-  base: "/log/",
-  
-    //<username>.github.io 뒤에 주소가 붙으시면 아래와 같이 ..
-    
-  };
+};
---
+++ docs/logbook/21-August/Aug-08.md
@@ -0,0 +1,20 @@
+---
+sidebarDepth: 0
+---
+
+# Aug-08
+
+## Time Log
+<mark>05:00-06:00</mark> Reading <br>
+<mark>07:00-17:00</mark> Work <br>
+<mark>19:00-21:00</mark> Workout <br>
+<mark>21:00-23:00</mark> Studying <br>
+
+## [JavaScript] Loops
+
+https://uiplaybook.dev/
+
+1. 툴팁
+   1. Focus되었을때도 보여야하고, Esc누르거나 다른곳 네비게이팅하면 사라진다.
+      1. 툴팁 자체는 포커스되면 안됨.
+   
